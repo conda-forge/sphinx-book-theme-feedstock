716deea21311177b5ce3460c56e7e0cca87103db
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-21 07:35:22 -0600

    fix tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,7 +3,7 @@ from subprocess import call
 
 FAIL_UNDER = "85"
 COV = ["coverage"]
-RUN = ["run", "--source=tox", "--branch", "-m"]
+RUN = ["run", "--source=sphinx_book_theme", "--branch", "-m"]
 PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
 REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
 
@@ -20,7 +20,7 @@ K = ["-k", f"not ({SKIP_OR})"]
 if __name__ == "__main__":
     sys.exit(
         # run the tests
-        call([*COV, *RUN, *PYTEST, *K], cwd="src/tests")
+        call([*COV, *RUN, *PYTEST, *K, "src/tests"])
         # maybe run coverage
-        or call([*COV, *REPORT], cwd="src/tests")
+        or call([*COV, *REPORT])
     )
